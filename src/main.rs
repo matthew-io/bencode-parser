be27d0935b9db1332da0bb7de327f0860775355f
matthew-io <xyzmonte@protonmail.com>
2025-05-27 22:34:48 +0100

    small change

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ mod bencode;
 fn main() {
     let contents = fs::read("./debian-12.11.0-amd64-netinst.iso.torrent")
         .expect("couldn't read torrent file");
-    let (bencode, _) = bencode::decode::decode(&contents).expect("couldn't decode given string");
+    let (bencode, _) = bencode::decode::decode(&contents)
+        .expect("couldn't decode given torrent");
     
-    let dict = if let Bencode::Dict(d) = bencode {
-        d
-    } else {
-        unimplemented!()
+    let dict = match bencode {
+        Bencode::Dict(d) => d,
+        _ => panic!("couldn't get top-level dictionary from torrent")
     };
     
     println!("{:#?}", dict.get(&b"announce"[..]));
